e8e0dea61945a6ce536a5e775fc5e1a33ccf712d
celikemirhan <celikemirhan770@gmail.com>
2025-08-18 01:12:09 +0300

    Add files via upload

---
+++ Breast_Cancer_KNN.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Read the CSV file (place your dataset here)
+df = pd.read_csv("data.csv")
+
+# Separate features and target variable
+data = df.loc[:, "radius_mean":"fractal_dimension_worst"]  # Features
+target = df["diagnosis"]  # Target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=42, test_size=0.3)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# List to store accuracy values for different k
+accuracy_values = []
+
+for k in range(1, 21):
+    knn = KNeighborsClassifier(n_neighbors=k)  # Create KNN model
+    knn.fit(X_train, y_train)  # Train the model on training data
+    y_pred = knn.predict(X_test)  # Predict on test data
+    accuracy = accuracy_score(y_test, y_pred)  # Calculate accuracy
+    accuracy_values.append(accuracy)  # Append accuracy to the list
+
+
+# Find the highest accuracy and corresponding k value
+best_value = max(accuracy_values)
+best_k = accuracy_values.index(best_value) + 1  # Add 1 because list index starts at 0
+
+
+# Train the model again with the best k
+knn = KNeighborsClassifier(n_neighbors=best_k)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(data)
+
