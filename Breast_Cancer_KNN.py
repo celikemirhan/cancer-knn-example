311087c2ab002f694a4f2375c965e7f2c1e052c6
celikemirhan <celikemirhan770@gmail.com>
2025-08-19 14:30:13 +0300

    Update Breast_Cancer_KNN.py
    conf matrix added

--- Breast_Cancer_KNN.py
+++ Breast_Cancer_KNN.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,confusion_matrix
 
 # Read the CSV file (place your dataset here)
 df = pd.read_csv("data.csv")
@@ -18,18 +18,20 @@ X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=4
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
-
+conf_matrix_list = []
 def trainModel(k):
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
     y_pred = knn.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
+    conf_accuracy = confusion_matrix(y_test,y_pred)
+    conf_matrix_list.append(conf_accuracy)
     return accuracy
 
 # List to store accuracy values for different k
 accuracy_values = []
 
-for k in range(1, 21): #find the highest accuracy
+for k in range(1, 21): # Hyperparameter tuning
 
     accuracy_values.append(trainModel(k))  # Append accuracy to the list
 
@@ -41,6 +43,7 @@ best_k = accuracy_values.index(best_value) + 1  # Add 1 because list index start
 
 # Train the model again with the best k
 final_accuracy = trainModel(best_k)
+
 
 
 
